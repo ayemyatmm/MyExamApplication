981e84a1f042f3d686d3928ffff70492856ff0f0
AyeMyatMyatPhyo <myominoo@mm.cybermissions.co.jp>
2020-12-22 15:27:48 +0900

    Firebaseを使うので、追加したのファイルをCommitしました。

--- app/src/main/java/jp/co/cybermissions/com/example/myexamapplication/MainFragment.kt
+++ app/src/main/java/jp/co/cybermissions/com/example/myexamapplication/MainFragment.kt
@@ -1,40 +1,107 @@
+/*
+ * Copyright 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package jp.co.cybermissions.com.example.myexamapplication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 import jp.co.cybermissions.com.example.myexamapplication.databinding.FragmentMainBinding
+import jp.co.cybermissions.com.example.myexamapplication.first.FirstFragmentDirections
 
 class MainFragment : Fragment() {
 
-    private lateinit var viewModel: MainViewModel
-//    private lateinit var communicator: communicator
+    companion object {
+        const val TAG = "MainFragment"
+        const val SIGN_IN_RESULT_CODE = 1001
+    }
+
+    // Get a reference to the ViewModel scoped to this Fragment
+    private val viewModel by viewModels<LoginViewModel>()
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val binding : FragmentMainBinding = DataBindingUtil.inflate(inflater
-        ,R.layout.fragment_main,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
 
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        binding.mainViewModel = viewModel
+        binding.loginButton.text = getString(R.string.login_btn)
 
-        binding.lifecycleOwner = viewLifecycleOwner
-
-//        communicator = activity as communicator
-
-        binding.takeExamButton.setOnClickListener{ view : View ->
-            view.findNavController().navigate(R.id.action_mainFragment_to_examFragment)
-//            communicator.passDataCom(viewModel.nameText.toString())
-        }
-            setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeAuthenticationState()
+
+        binding.examBtn.setOnClickListener {
+            val action = MainFragmentDirections.actionMainFragmentToFirstFragment()
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun observeAuthenticationState() {
+
+        viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
+            when (authenticationState) {
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                    binding.welcomeText.text = FirebaseAuth.getInstance().currentUser?.displayName + " are logged in."
+
+                    binding.loginButton.text = getString(R.string.logout_button_text)
+                    binding.loginButton.setOnClickListener {
+                        AuthUI.getInstance().signOut(requireContext())
+                    }
+                    binding.examBtn.isVisible = true
+                }
+                else -> {
+                    binding.welcomeText.text = "Welcome to My Exam App"
+
+                    binding.loginButton.text = getString(R.string.login_button_text)
+                    binding.loginButton.setOnClickListener {
+                        launchSignInFlow()
+                    }
+                    binding.examBtn.isVisible = false
+                }
+            }
+        })
+    }
+
+
+    private fun launchSignInFlow() {
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+                providers
+            ).build(), MainFragment.SIGN_IN_RESULT_CODE
+        )
     }
 }
--- app/src/main/java/jp/co/cybermissions/com/example/myexamapplication/score/ScoreViewModel.kt
+++ app/src/main/java/jp/co/cybermissions/com/example/myexamapplication/score/ScoreViewModel.kt
@@ -28,13 +28,4 @@ class ScoreViewModel(finalScore: Int) : ViewModel() {
             _result.value = "Fail"
         }
     }
-
-//    fun getFactToDisplay(context: Context): String {
-//        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-//        val factTypePreferenceKey = context.getString(R.string.preference_fact_type_key)
-//        val defaultFactType = context.resources.getStringArray(R.array.fact_type)[0]
-//        val funFactType = sharedPreferences.getString(factTypePreferenceKey, defaultFactType)
-//
-//        return LoginViewModel.androidFacts[Random.nextInt(0, LoginViewModel.androidFacts.size)]
-//    }
 }
