f2a6746bd4330c4248c9331ba3ced5982138f1c5
AyeMyatMyatPhyo <myominoo@mm.cybermissions.co.jp>
2020-12-23 18:26:18 +0900

    Firebaseに受けたデータを保存するのを追加しました。

--- app/src/main/java/jp/co/cybermissions/com/example/myexamapplication/score/ScoreFragment.kt
+++ app/src/main/java/jp/co/cybermissions/com/example/myexamapplication/score/ScoreFragment.kt
@@ -1,17 +1,16 @@
 
 package jp.co.cybermissions.com.example.myexamapplication.score
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.google.firebase.auth.FirebaseAuth
-import jp.co.cybermissions.com.example.myexamapplication.LoginViewModel
+import com.google.firebase.database.FirebaseDatabase
 import jp.co.cybermissions.com.example.myexamapplication.R
 import jp.co.cybermissions.com.example.myexamapplication.databinding.ScoreFragmentBinding
 
@@ -19,21 +18,33 @@ class ScoreFragment : Fragment() {
 
     private lateinit var viewModel: ScoreViewModel
     private lateinit var viewModelFactory: ScoreViewModelFactory
+    var currentUser = FirebaseAuth.getInstance().currentUser?.displayName
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-      val binding = DataBindingUtil.inflate<ScoreFragmentBinding>(inflater,
-      R.layout.score_fragment,container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+      val binding = DataBindingUtil.inflate<ScoreFragmentBinding>(
+          inflater,
+          R.layout.score_fragment, container, false
+      )
 
         viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(requireArguments()).score)
-        viewModel = ViewModelProvider(this,viewModelFactory)
+        viewModel = ViewModelProvider(this, viewModelFactory)
             .get(ScoreViewModel::class.java)
         binding.scoreViewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
-        binding.homebutton.setOnClickListener{
-            view: View -> view.findNavController().navigate(ScoreFragmentDirections.actionScoreFragmentToMainFragment())
+        binding.homebutton.setOnClickListener{ view: View -> view.findNavController().navigate(
+            ScoreFragmentDirections.actionScoreFragmentToMainFragment()
+        )
+            val mDatabase = FirebaseDatabase.getInstance()
+            val mDatabaseReference = mDatabase.getReference()
+            mDatabaseReference.child("Exam").push().setValue(currentUser);
+            mDatabaseReference.child("Exam").push().setValue(viewModel.score);
         }
+
+        binding.yourDisplayText.text = "Hey "+ currentUser
         return binding.root
 
     }
